a2d696a3218fd730e6f19c5d03de0aa869d8e0d4
shanecmdexter <94471217+shanecmdexter@users.noreply.github.com>
2022-01-14 17:03:35 +0000

    fixed invalid env vars
    - was checking for environment variables specified for ICBS. Removed test credentials in favour of live deployment keys

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,6 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
-const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL_ICBS as string
-const VITE_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY_ICBS as string
-console.log('VITE_SUPABASE_URL: ', VITE_SUPABASE_URL)
-console.log('VITE_SUPABASE_ANON_KEY: ', VITE_SUPABASE_ANON_KEY)
-export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY)
+// let isDev = import.meta.env.DEV
+// const VITE_SUPABASE_URL = isDev ?
+//     import.meta.env.VITE_SUPABASE_URL as string
+//     : import.meta.env.VITE_SUPABASE_URL_ICBS as string
+
+// const VITE_SUPABASE_ANON_KEY = isDev ?
+//     import.meta.env.VITE_SUPABASE_ANON_KEY as string
+//     : import.meta.env.VITE_SUPABASE_ANON_KEY_ICBS as string
+
+// console.log('VITE_SUPABASE_URL: ', VITE_SUPABASE_URL)
+// console.log('VITE_SUPABASE_ANON_KEY: ', VITE_SUPABASE_ANON_KEY)
+
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string
+const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+export const supabase = createClient(supabaseURL, anonKey)
