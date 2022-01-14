995413e83698d04d5668070c4f04a5fe82623417
shanecmdexter <94471217+shanecmdexter@users.noreply.github.com>
2022-01-14 16:53:10 +0000

    logging keys

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL_ICBS as string
 const VITE_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY_ICBS as string
+console.log('VITE_SUPABASE_URL: ', VITE_SUPABASE_URL)
+console.log('VITE_SUPABASE_ANON_KEY: ', VITE_SUPABASE_ANON_KEY)
 export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY)
