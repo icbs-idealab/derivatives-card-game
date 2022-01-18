30aaf4d0a6fc3e3216db6f8d2d5fed929fcdc8f4
shanecmdexter <94471217+shanecmdexter@users.noreply.github.com>
2022-01-18 11:27:20 +0000

    account verified route

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -255,7 +255,7 @@
 
     })
 
-    let nonRedirectPaths = ['/', 'admin', '/update-password']
+    let nonRedirectPaths = ['/', 'admin', '/update-password', '/account-verified']
 
     afterUpdate(() => {
         if(authChecked && !activeUser.id && nonRedirectPaths.indexOf($page.path) === -1){
---
+++ src/routes/account-verified.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+</script>
+
+<div class="flex fd-col">
+    <h1>Congratulations</h1>
+    <p>Your account has been verified and you may now participate in games.</p>
+    <br>
+    <a class="faux-button" href="/">Find or Create a Game</a>
+</div>
+
+<style>
+    div {
+        min-height: 100%;
+        min-width: 100%;
+        width: 100%;
+        height: 100vh;
+    }
+    a.faux-button {
+        padding: 18px 24px;
+        display: inline;
+        background: black;
+        border-radius: 4px;
+        color: white!important;
+        text-decoration: none!important;
+    }
+
+    a:hover{
+        opacity: 0.85;
+    }
+</style>
